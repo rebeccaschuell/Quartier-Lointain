2c13c48b9d3a0ea76dfcb7e8646b9e3d4d07170a
rebeccaschuell <rebecca.schuell@studentfr.ch>
2023-11-14 11:52:47 +0100

    Integration de la base du code pour la section de défilement horizontal

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// import { gsap } from "gsap";
+
+// gsap.to(".slider-track", {
+//   scrollTrigger: {
+//     trigger: ".slider-mask",
+//     start: "top top",
+//     end: "+=300%",
+//     pin: true,
+//     scrub: true,
+//     markers: true,
+//   },
+//   x: "-75%",
+//   ease: "sine.inOut",
+// });
