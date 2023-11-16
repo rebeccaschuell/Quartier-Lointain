1333a0cbe47d42244f55fa651916e5d6e9842ebf
rebeccaschuell <rebecca.schuell@studentfr.ch>
2023-11-16 14:08:36 +0100

    Animation de la frise de vie d'Hiroshi avec gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,39 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.set(".slider-track", {
+  x: "-52.5%",
+});
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".slider-mask",
+    start: "top top",
+    //   endTrigger: ".slider-mask",
+    end: "+=3000",
+    //   end: "bottom 50%+=100px",
+  },
+});
+
+tl.to(".slider-track", {
+  x: "-95.4%",
+  delay: 2,
+  duration: 6,
+});
+
+tl.to(".slider-track", {
+  x: "0",
+  delay: 2,
+  duration: 2,
+});
+
+tl.to(".slider-track", {
+  x: "-26%",
+  delay: 2,
+  duration: 6,
+});
 
 // gsap.to(".slider-track", {
 //   scrollTrigger: {
